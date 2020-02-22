0dd46c745fd86d76760186995003f7b170d59075
Alistar542 <alistarfz@gmail.com>
2020-02-22 06:45:29 -0800

    UI Changes supporting extra fields

--- src/components/Login/LoginComponent.js
+++ src/components/Login/LoginComponent.js
@@ -2,114 +2,117 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import {useAuth} from './context/auth';
+import { useAuth } from './context/auth';
 import { Redirect } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import BookBackground from '../../photos/login_background.jpg'
 
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         marginTop: theme.spacing(1),
+        height: '100%',
     },
     paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        background:'white',
+        padding:'10%',
+        borderRadius:'1%',
+        marginBottom:theme.spacing(30)
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
+        margin: theme.spacing(3, 0, 2),
     },
-    paper_div:{
-      background:'blue'
+    paper_div: {
+        background: 'blue'
+    },
+    back: {
+        height: '100%',
+        display: 'flex',
+        //backgroundImage : `url(${BookBackground})`,
+        marginBottom:theme.spacing(1),
+        backgroundPosition:'center',
+        background:'#00bcd4'
     }
-  }));
+}));
 
-  
 
- 
 
- 
 
-export default function LoginComponent(){
-    const [username,setUsername]=React.useState();
-    const [password,setPassword]=React.useState();
+
+
+
+export default function LoginComponent() {
+    const [username, setUsername] = React.useState();
+    const [password, setPassword] = React.useState();
     const [isLoggedIn, setLoggedIn] = React.useState(false);
     const { setAuthTokens } = useAuth();
     const classes = useStyles();
 
 
-    const onChangeUserName=(event)=>{
+    const onChangeUserName = (event) => {
         event.preventDefault();
         setUsername(event.target.value.toLowerCase())
     }
-  
-    const onChangeUserPassword=(event)=>{
-      event.preventDefault();
-      setPassword(event.target.value)
-  }
 
-  
-  
-  const handleSubmit=(event)=>{
-      event.preventDefault();
-      if(username==='admin' && password==='admin'){
-        setLoggedIn(true);
-        setAuthTokens({
-          user:username,
-        });
-      }
-      if(username==='alistar' && password==='alistar'){
-        setLoggedIn(true);
-        setAuthTokens({
-          user:username,
-        });
-      }
-  
-  }
+    const onChangeUserPassword = (event) => {
+        event.preventDefault();
+        setPassword(event.target.value)
+    }
 
-  if (isLoggedIn) {
-    return <Redirect to="/home" />;
-  }
 
-    return(
-      <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.root} Validate autoComplete="off" onSubmit={handleSubmit}>
-            <TextField
-                required
-                fullWidth
-                id="outlined-required"
-                label="Username"
-                variant="outlined"
-                margin="normal"
-                onChange={onChangeUserName}
-                value={username}
-            />
-            
-            <TextField
-                required
-                fullWidth
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                margin="normal"
-                onChange={onChangeUserPassword}
-            />
-            <br></br>
-            <Button 
-            fullWidth
-            variant="contained"
-            className={classes.submit}
-            color="primary" type="submit"> LOGIN </Button>
-        </form>
-        </div>
-        </Container>
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (username === 'admin' && password === 'admin') {
+            setLoggedIn(true);
+            setAuthTokens({
+                user: username,
+            });
+        }
+        if (username === 'alistar' && password === 'alistar') {
+            setLoggedIn(true);
+            setAuthTokens({
+                user: username,
+            });
+        }
+
+    }
+
+    if (isLoggedIn) {
+        return <Redirect to = "/home" / > ;
+    }
+
+    return ( <div className = { classes.back } >
+        <Container component = "main" maxWidth = "xs" >
+        <div className = { classes.paper } >
+        <Typography component = "h1" variant = "h5" >
+        Sign in
+        </Typography> 
+        <form className = { classes.root }
+        Validate autoComplete = "off"
+        onSubmit = { handleSubmit } >
+        <TextField required fullWidth id = "outlined-required"
+        label = "Username"
+        variant = "outlined"
+        margin = "normal"
+        onChange = { onChangeUserName }
+        value = { username }/>
+
+        <TextField required fullWidth id = "outlined-password-input"
+        label = "Password"
+        type = "password"
+        autoComplete = "current-password"
+        variant = "outlined"
+        margin = "normal"
+        onChange = { onChangeUserPassword }/> 
+        <br></br> 
+        <Button fullWidth variant = "contained" className = { classes.submit }
+        color = "primary"
+        type = "submit"> LOGIN </Button> </form> </div> </Container> </div>
     );
 }
--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -11,8 +11,14 @@ import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import SuccessDialog from './Dialogs/SuccessDialog';
 import FailDialog from './Dialogs/FailDialog';
-import {useAuth} from './Login/context/auth'
-
+import {useAuth} from './Login/context/auth';
+import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { flexbox } from '@material-ui/system';
+import FormControl from '@material-ui/core/FormControl';
+import Divider from '@material-ui/core/Divider';
 
 
 const useStyles = makeStyles(theme => ({
@@ -22,6 +28,17 @@ const useStyles = makeStyles(theme => ({
       width: 200,
     },
   },
+  personalInfoDiv:{
+        margin: theme.spacing(1),
+        display:'flex',
+        flexDirection:'row',
+        flexWrap:'wrap',
+        alignItems: 'stretch'
+  },
+  formControlSelect:{
+    margin: theme.spacing(1),
+    minWidth: 160,
+  }
 }));
 
 
@@ -37,6 +54,18 @@ export const UserComponent = () => {
   const [interestedCourse, setInterestedCourse] = React.useState('');
   const [dialogState,setDialogState]=React.useState(false);
   const [successOrFail, setSuccessOrFail] = React.useState(false);
+  const [maritalStatus,setMaritalStatus] = React.useState();
+  const [email,setEmail] = React.useState();
+  const [middleName,setMiddleName] = React.useState();
+  const [selectedDateOfBirth,setSelectedDateOfBirth] = React.useState(null);
+  const [gender,setGender] = React.useState();
+  const [englishExamType,setEnglishExamType] = React.useState();
+  const [overall,setOverall] = React.useState();
+  const [listening,setListening] = React.useState();
+  const [reading,setReading] = React.useState();
+  const [writing,setWriting] = React.useState();
+  const [speaking,setSpeaking] = React.useState();
+  const [selectedExamDate,setExamDate] = React.useState(null);
 
   const handleDateChange = date => {
     if(date)
@@ -96,17 +125,111 @@ export const UserComponent = () => {
     setPhoneNumber(event.target.value);
   }
 
+  const onChangeEmail = event =>{
+    setEmail(event.target.value);
+  }
+
+  const onChangeMiddleName = event =>{
+    setMiddleName(event.target.value);
+  }
+
+  const onChangeDOB = date => {
+    if(date)
+    setSelectedDateOfBirth(new Date(date.getFullYear(),date.getMonth(),date.getDate()));
+  };
+
+  const onChangeGender = event => {
+    setGender(event.target.value);
+  }
+
+  const onChangeMaritalStatus = event =>{
+    setMaritalStatus(event.target.value);
+  }
+
+  const onChangeListening = event => {
+
+  }
+
+  const onChangeWriting = event => {
+
+  }
+
+  const onChangeReading = event => {
+
+  }
+
+  const onChangeOverall = event => {
+
+  }
+
+  const onChangeSpeaking = event => {
+
+  }
+
+  const onChangeExamDate = date => {
+    if(date)
+    setExamDate(new Date(date.getFullYear(),date.getMonth(),date.getDate()));
+  }
+
+  const onChangeEnglishExamType = event => {
+    
+  }
+
   return(
              
     <div>
 
     <form className={classes.root} Validate autoComplete="off" onSubmit={handleSubmit}>
-        Add a student
-    <div>
+        <Typography component = "h6" variant = "h6" >
+          Personal Information
+        </Typography> 
+    <div className={classes.personalInfoDiv}>
       <TextField required id="standard-required" label="First Name" value={firstName} onChange={onChangeFirstName}/>
+      <TextField required id="standard-required" label="Middle Name" value={middleName} onChange={onChangeMiddleName}/>
       <TextField required id="standard-required" label="Last Name" value={lastName} onChange={onChangeLastName}/>
-      <TextField required id="standard-required" label="Interested Course" value={interestedCourse} onChange={onChangeInterestedCourse}/>
+      <TextField required id="standard-required" label="Email" value={email} onChange={onChangeEmail}/>
       <TextField required id="standard-required" label="Phone Number" value={phoneNumber} onChange={onChangePhoneNumber}/>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date Of Birth"
+          value={selectedDateOfBirth}
+          onChange={onChangeDOB}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        </MuiPickersUtilsProvider>
+        <FormControl className={classes.formControlSelect}>
+        <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={gender}
+              onChange={onChangeGender}
+            >
+              <MenuItem value={'M'}>Male</MenuItem>
+              <MenuItem value={'F'}>Female</MenuItem>
+              <MenuItem value={'O'}>Other</MenuItem>
+            </Select>
+            </FormControl>
+            <FormControl className={classes.formControlSelect}>
+            <InputLabel id="demo-simple-select-label">Marital Status</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={maritalStatus}
+              onChange={onChangeMaritalStatus}
+            >
+              <MenuItem value={'M'}>Married</MenuItem>
+              <MenuItem value={'F'}>Un-Married</MenuItem>
+            </Select>
+            </FormControl>
+      <TextField required id="standard-required" label="Interested Course" value={interestedCourse} onChange={onChangeInterestedCourse}/>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <KeyboardDatePicker
           disableToolbar
@@ -124,6 +247,48 @@ export const UserComponent = () => {
         </MuiPickersUtilsProvider>
 
       </div>
+      <br></br>
+      <Divider/>
+      <br></br>
+      <Typography component = "h6" variant = "h6" >
+          English Exam Type
+        </Typography> 
+        <div className={classes.personalInfoDiv}>
+        <FormControl className={classes.formControlSelect}>
+            <InputLabel id="demo-simple-select-label">English Exam Type</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={englishExamType}
+              onChange={onChangeEnglishExamType}
+            >
+              <MenuItem value={'IELTS'}>IELTS</MenuItem>
+              <MenuItem value={'TOEFL'}>TOEFL</MenuItem>
+              <MenuItem value={'None'}>None</MenuItem>
+            </Select>
+            </FormControl>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Exam Date"
+          value={selectedExamDate}
+          onChange={onChangeExamDate}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        </MuiPickersUtilsProvider>
+          <TextField required id="standard" label="Overall" value={overall} onChange={onChangeOverall}/>
+          <TextField required id="standard" label="Listening" value={listening} onChange={onChangeListening}/>
+          <TextField required id="standard" label="Reading" value={reading} onChange={onChangeReading}/>
+          <TextField required id="standard" label="Writing" value={writing} onChange={onChangeWriting}/>
+          <TextField required id="standard" label="Speaking" value={speaking} onChange={onChangeSpeaking}/>
+        </div>
+      <br></br>
       <Button variant="contained" color="primary" type="submit"> Save </Button>
        </form>
        {successOrFail?<SuccessDialog dialogState={dialogState} setDialogStateFn={setDialogState}/>
@@ -131,4 +296,3 @@ export const UserComponent = () => {
     </div>         
   );
 }
-
